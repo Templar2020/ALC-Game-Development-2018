b8ffd083085d2bfa033b13b80dcfab96d228c7e5
Unknown <preston.powell@uvu.edu>
2017-11-27 13:55:44 -0700

    Game Update 11/27/17

--- Ghost House/Assets/Scripts/FlashLight.cs
+++ Ghost House/Assets/Scripts/FlashLight.cs
@@ -8,11 +8,19 @@ using UnityEngine;
 public class FlashLight : MonoBehaviour {
 
 	public bool lightOn = true;
+	// Flashlight power capacity
+	public int maxPower = 4;
+	// Useable flashlight power
+	public int currentPower;
 
 	Light light;
 
 	// Use this for initialization
 	void Start () {
+		//Add power to flashlight
+		currentPower = maxPower;
+		print("Power = " + currentPower);
+
 		light = GetComponent<Light> ();
 		// Set Light default to ON
 		lightOn = true;
